5596b6e4c0fa27bba46a23e9924c0bda88a292dc
Georgi Lambov <georgi.lambov@gmail.com>
2021-09-09 13:15:25 +0300

    zfs pools, zcache and basic system setup.

--- archiso-provision.sh
+++ archiso-provision.sh
@@ -4,4 +4,43 @@ echo install zfs module
 curl -s https://eoli3n.github.io/archzfs/init | bash
 
 echo parted
-parted --script /dev/vda mklabel gpt mkpart primary 5MB% 512MB mkpart primary 512MB 100%  set 1 boot on set 1 esp on
+parted --script /dev/vda mklabel gpt mkpart primary 5MB% 512MB mkpart primary 512MB 100% set 1 boot on set 1 esp on
+
+echo zpool setup
+zpool create -f -o ashift=12 \
+    -O acltype=posixacl \
+    -O relatime=on \
+    -O xattr=sa \
+    -O dnodesize=legacy \
+    -O normalization=formD \
+    -O mountpoint=none \
+    -O canmount=off \
+    -O devices=off \
+    -R /mnt \
+    zroot /dev/vda2
+
+zfs create -o mountpoint=none zroot/data
+zfs create -o mountpoint=none zroot/ROOT
+zfs create -o mountpoint=/ -o canmount=noauto zroot/ROOT/default
+zfs create -o mountpoint=/home zroot/data/home
+
+zfs unmount -a
+zfs mount zroot/ROOT/default
+
+echo cache
+mkdir -p  /mnt/etc/zfs
+zpool set cachefile=/etc/zfs/zpool.cache zroot
+cp /etc/zfs/zpool.cache /mnt/etc/zfs/zpool.cache
+
+echo create boot partition
+mkfs.vfat /dev/vda1
+mkdir /mnt/boot
+mount /dev/vda1 /mnt/boot
+
+echo filesystem table
+genfstab -U -p /mnt >> /mnt/etc/fstab
+
+echo basic system setup
+pacstrap /mnt base base-devel linux linux-headers linux-firmware grub efibootmgr \
+  vim zsh openssh
+  
