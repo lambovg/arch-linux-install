cc76edf1e7d001f02e3745a79db9a34fd5d17a83
Georgi Lambov <georgi.lambov@gmail.com>
2021-09-09 12:49:50 +0300

    parted script

--- archiso-provision.sh
+++ archiso-provision.sh
@@ -4,3 +4,4 @@ echo install zfs module
 curl -s https://eoli3n.github.io/archzfs/init | bash
 
 echo parted
+parted --script /dev/sda mklabel gpt mkpart primary 5MB% 512MB mkpart primary 512MB 100%  set 1 boot on set 1 esp on
