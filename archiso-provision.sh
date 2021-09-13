c2a4f1c38abfdb4e78305c48546ed8a3576da955
Georgi Lambov <georgi.lambov@gmail.com>
2021-09-13 17:31:06 +0300

    arch on ext4.

--- archiso-provision.sh
+++ archiso-provision.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-echo install zfs module
-curl -s https://eoli3n.github.io/archzfs/init | bash
+# echo install zfs module
+# curl -s https://eoli3n.github.io/archzfs/init | bash
 
 echo parted
-parted --script /dev/vda mklabel gpt mkpart primary 5MB% 512MB mkpart primary 512MB 100% set 1 boot on set 1 esp on
+parted --script -a opt /dev/vda mklabel gpt mkpart primary 5MB% 512MB mkpart primary 512MB 100% set 1 boot on set 1 esp on
 
 echo zpool setup
 # zpool create -f -o ashift=12 \
 #     -O acltype=posixacl \
 #     -O relatime=on \
 #     -O xattr=sa \
-#     -O dnodesize=legacy \
+#     -O dnodesize=auto \
 #     -O normalization=formD \
 #     -O mountpoint=none \
 #     -O canmount=off \
@@ -19,33 +19,38 @@ echo zpool setup
 #     -R /mnt \
 #     zroot /dev/vda2
 
-zpool create -f -o ashift=12 \
-    -O acltype=posixacl \
-    -O canmount=off \
-    -O dnodesize=auto \
-    -O normalization=formD \
-    -O atime=off \
-    -O xattr=sa \
-    -O mountpoint=none \
-    -R /mnt \
-    zroot /dev/vda2
+# zpool create -f -o ashift=12 \
+#     -O acltype=posixacl \
+#     -O canmount=off \
+#     -O dnodesize=auto \
+#     -O normalization=formD \
+#     -O atime=off \
+#     -O xattr=sa \
+#     -O mountpoint=none \
+#     -R /mnt \
+#     zroot /dev/vda2
 
-zfs create -o mountpoint=none zroot/data
-zfs create -o mountpoint=none zroot/ROOT
-zfs create -o mountpoint=/ -o canmount=noauto zroot/ROOT/default
-zfs create -o mountpoint=/home zroot/data/home
+# zfs create -o mountpoint=none zroot/data
+# zfs create -o mountpoint=none zroot/ROOT
+# zfs create -o mountpoint=/ -o canmount=noauto zroot/ROOT/default
+# zfs create -o mountpoint=/home zroot/data/home
 
-#zfs create -o canmount=noauto -o mountpoint=/ zroot/rootfs
-#zfs create -o mountpoint=/home zroot/rootfs/home
+# zfs create -o canmount=noauto -o mountpoint=/ zroot/rootfs
+# zfs create -o mountpoint=/home zroot/rootfs/home
 
-zfs unmount -a
-rm -rf /mnt/*
-zfs mount zroot/ROOT/default
+# setopt RM_STAR_SILENT
+# zfs unmount -a
+# rm -rf /mnt/*
+# zfs mount zroot/ROOT/default
 
-echo cache
-mkdir -p /mnt/etc/zfs
-zpool set cachefile=/etc/zfs/zpool.cache zroot
-cp /etc/zfs/zpool.cache /mnt/etc/zfs/zpool.cache
+# echo cache
+# mkdir -p /mnt/etc/zfs
+# zpool set cachefile=/etc/zfs/zpool.cache zroot
+# cp /etc/zfs/zpool.cache /mnt/etc/zfs/zpool.cache
+
+echo create root partition
+mkfs.ext4 /dev/vda2
+mount /dev/vda2 /mnt
 
 echo create boot partition
 mkfs.vfat /dev/vda1
@@ -56,7 +61,6 @@ echo filesystem table
 genfstab -U -p /mnt >>/mnt/etc/fstab
 
 echo basic system setup
-#pacstrap /mnt base base-devel linux linux-headers linux-firmware grub efibootmgr \
-#    vim zsh openssh
-
-pacstrap /mnt base base-devel grub-efi-x86_64 zsh vim efibootmgr openssh tmux git gnupg rsync wget curl sudo
+sed -i -e 's/CheckSpace/#CheckSpace/' /etc/pacman.conf
+pacstrap /mnt base base-devel linux linux-headers linux-firmware \
+    grub zsh vim efibootmgr openssh tmux git gnupg rsync wget curl sudo
